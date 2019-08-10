1a58c6f2ad323de448c8caebfb7c74c3696c2ea1
t572wai <53143353+t572wai@users.noreply.github.com>
2019-08-10 19:20:56 +0900

    aaa

--- scroll.js
+++
@@ -1,9 +0,0 @@
-let i=0;
-let scroll = 1000;
-while (true) {
-	i=1
-	setTimeout(function () {
-		updateText(i);
-	},scroll);
-	i++;
-}
--- worker-dom-master/CODE_OF_CONDUCT.md
+++
@@ -1,1 +0,0 @@
-See https://github.com/ampproject/meta/blob/master/CODE_OF_CONDUCT.md
